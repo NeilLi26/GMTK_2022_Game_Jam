ff28bb71376d394a08f7e6ff461e65fcdb874ab7
whalebeavercat <60767480+whalebeavercat@users.noreply.github.com>
2022-07-15 20:58:25 -0600

    Added Simple Health Bar Prefab and Edits to SampleScene
    Added Health Bar Simple Changes to Sample Scene

---
+++ Assets/Scripts/FillSliderBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillSliderBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerHealth playerHealth;
+    public Image fillImage;
+    private Slider slider;
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value <= slider.minValue) // whether the slider is too small
+        {
+            fillImage.enabled = false;
+        }
+        if (slider.value > slider.minValue && !fillImage.enabled) // change to the health bar
+        {
+            fillImage.enabled = true;
+        }
+        float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
+        if (slider.value <= slider.maxValue / 2)
+        {
+            fillImage.color = Color.red;   // for a change in color
+        }
+        else if (fillValue > slider.maxValue / 2)
+        {
+            fillImage.color = Color.green;
+        }
+        slider.value = fillValue;
+        //  this renders the player health bar
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// HEALTH
+   public class PlayerHealth : MonoBehaviour
+    {
+        public float currentHealth = 6f;
+        public float maxHealth = 6f;
+    }
+
+
