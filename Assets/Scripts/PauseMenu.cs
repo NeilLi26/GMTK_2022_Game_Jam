136bb799daab4d912b1f39b8cdbabecf98f393fd
whalebeavercat <60767480+whalebeavercat@users.noreply.github.com>
2022-07-17 01:58:58 -0600

    Added HealthBar UI to the levels
    added them

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,19 +7,37 @@ public class PauseMenu : MonoBehaviour
 {
 
     [SerializeField] GameObject pauseMenu;
+    private bool pauseMenuOn;
+
+    void Start() {
+        pauseMenuOn = false;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!pauseMenuOn) {
+                Pause();
+            } else {
+                Resume();
+            }
+        }
+    }
 
     public void Pause() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        pauseMenuOn = true;
     }
 
     public void Resume() {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        pauseMenuOn = false;
     }
 
     public void Home(int sceneID) {
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
+        pauseMenuOn = false;
     }
 }
