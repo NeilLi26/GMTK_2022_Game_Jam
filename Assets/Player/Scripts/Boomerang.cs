d82f816387bc28a899a81ac9035a3e6a82be621b
Rosy <89042585+rosy-j@users.noreply.github.com>
2022-07-16 17:33:36 -0700

    edited boomerang

--- Assets/Player/Scripts/Boomerang.cs
+++ Assets/Player/Scripts/Boomerang.cs
@@ -9,63 +9,49 @@ public class Boomerang : MonoBehaviour {
     [SerializeField] private float attackRange;
     [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float boomerangDistance;
     public Camera camera;
-    private bool hasReturned;
+    private bool hasHit;
+    private float lifetime;
 
     void Start() {
-        hasReturned = true;
+        hasHit = false;
+        lifetime = 0;
     }
 
     private void Update() {
+        if (hasHit || lifetime >= maxTime) {
+            gameObject.SetActive(false);
+            return;
+        }
+
+
         Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
         foreach (Collider2D enemy in enemies) {
+            hasHit = true;
             // TODO: damage enemy
             Debug.Log("hit enemy");
-        }
-        Collider2D player = Physics2D.OverlapCircle(transform.position, attackRange, playerLayer);
-        if (!hasReturned && player != null) {
-            hasReturned = true;
-            gameObject.SetActive(false);
         }
     }
 
     public IEnumerator boomerangAttack() {
-        
+
         transform.position = player.transform.position;
         Vector3 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = transform.position.z;
 
-        while (transform.position != mousePosition) {
+        lifetime = 0;
+
+        while (!hasHit && lifetime < maxTime) {
+            lifetime += Time.deltaTime;
             Vector3 currentPos = transform.position;
-            float time = Vector3.Distance(currentPos, player.position) / (boomerangSpeed) * Time.deltaTime;
+            float time = Vector3.Distance(currentPos, mousePosition) / (boomerangSpeed) * Time.deltaTime;
 
             transform.position = Vector3.MoveTowards(currentPos, mousePosition, time);
 
             yield return null;
         }
 
-        StartCoroutine(boomerangReturn());
-        
-    }
-
-    public IEnumerator boomerangReturn() {
-        hasReturned = false;
-
-        float timer = 0;
-
-        while (!hasReturned && timer < maxTime) {
-            Vector3 currentPos = transform.position;
-            timer += Time.deltaTime;
-            Debug.Log(timer);
-            float time = Vector3.Distance(currentPos, player.position) / (boomerangSpeed) * Time.deltaTime;
-
-            transform.position = Vector3.MoveTowards(currentPos, player.position, time);
-
-            yield return null;
-        }
-
-        hasReturned = true;
-        gameObject.SetActive(false);
     }
 
 }
