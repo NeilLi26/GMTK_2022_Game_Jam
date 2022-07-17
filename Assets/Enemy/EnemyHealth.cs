3758129eb7eaf495e81916a6a635941205afe20d
Kevin Liu <83846217+Kevin-L90@users.noreply.github.com>
2022-07-17 01:28:15 -0700

    Add files via upload

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -6,8 +6,10 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] public float maxHealth;
     public float currHealth;
+    public bool Invincible;
 
     void Start() {
+        Invincible = false;
         currHealth = maxHealth;
     }
 
@@ -22,15 +24,33 @@ public class EnemyHealth : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Player") {
-            // TODO: update this value?
-            TakeDamage(1);
-        }
+        // if (collision.gameObject.tag == "Enemy") {
+        //     // TODO: update this value?
+        //     TakeDamage(1);
+        // }
     }
 
     public void TakeDamage(float damage) {
-        currHealth -= damage;
-        currHealth = Mathf.Clamp(currHealth, 0, maxHealth);
-        Debug.Log(currHealth);
+        if (!Invincible)
+        {
+            currHealth -= damage;
+            currHealth = Mathf.Clamp(currHealth, 0, maxHealth);
+            Debug.Log(currHealth);
+
+            StartIFrame();
+        }
+    }
+
+    public void StartIFrame()
+    {
+        Debug.Log(Invincible);
+        // TODO: invincible animation
+        Invincible = true;
+        Invoke("EndIFrame", 1);
+    }
+
+    void EndIFrame()
+    {
+        Invincible = false ;
     }
 }
